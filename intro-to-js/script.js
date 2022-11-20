5c4fa8a87f65716a76ea77055c3b56894c0e1af8
kalicialopez <104682798+kalicialopez@users.noreply.github.com>
2022-11-20 23:14:05 +0800

    exercises
    completion of all prerequisite html and css work

--- intro-to-js/script.js
+++ intro-to-js/script.js
@@ -1,29 +1,38 @@
-window.onload = function() {
-    // start
+window.onload = function () {
+  let starterDiv = document.getElementById("starter-div");
+  let firstHeading = document.createElement("h2");
 
-    // end
+  firstHeading.innerText = "This is an H2 created with JS";
+  firstHeading.classList.add("js-header");
+
+  starterDiv.appendChild(firstHeading);
+
+  let secondHeading = document.createElement("h2");
+  secondHeading.innerText = "This is another H2 created with JS";
+  secondHeading.classList.add("js-header");
+  secondHeading.id = "second-h2";
+
+  starterDiv.appendChild(secondHeading);
+};
+
+function showName() {
+  console.log("showName");
+
+  let name = document.getElementById("name").value;
+  document.getElementById("greeting-text").innerText = "Hi " + name;
 }
 
-
-function showName(){
-    console.log('showName');
-    // start
-
-    // end
+function changeColor(color) {
+  document.body.style.backgroundColor = color;
 }
 
-function changeColour(colour) {
-    // start
-
-    // end
-}
-
-function resetColour() {
-    document.body.style.backgroundColor = '#FCE166';
+function resetColor() {
+  document.body.style.backgroundColor = "#FCE166";
 }
 
 function toggleLightbulb() {
-    // start
-
-    // end
+  // start
+  let lightbulb = document.getElementById("light");
+  lightbulb.classList.toggle("on");
+  // end
 }
